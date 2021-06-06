9d9241750ee88c8659e5e7e75aa17b4b80a7e105
Lev Popov <levpopov1@gmail.com>
2021-06-06 17:44:20 +0100

    Adds 404 view

---
+++ src/views/NotFound.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+function NotFound() {
+  return (
+    <div className="404">
+      <h1>404 - Page Not Found!</h1>
+      <Link to="/">
+        Go Home
+      </Link>
+    </div>
+  )
+}
+
+export default NotFound
