4b2d3cb637f532650eaffadd9f76a547650022fd
Lev Popov <levpopov1@gmail.com>
2021-06-06 16:47:39 +0100

    applies link tags from react router

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,128 +1,116 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Sidebar() {
   return (
-    <nav class="sidebar main-bg-color-vertical">
-      <div class="sidebar-sticky">
-        <ul class="nav flex-column">
-          <p class="sidebar-navblock-header">
-            Favourites
+    <nav className="sidebar main-bg-color-vertical">
+      <div className="sidebar-sticky">
+        <ul className="nav flex-column">
+          <p className="sidebar-navblock-header">
+            Section Header
           </p>
-          <li class="nav-item">
-            <a class="nav-link d-flex align-items-center" href="">
-              <i class="bi bi-star-fill"></i>
-              <span class="label">itemName</span>
-              <i class="bi bi-chevron-right ms-auto"></i>
-            </a>
+          <li className="nav-item">
+            <Link className="nav-link d-flex align-items-center" to="/location">
+              <i className="bi bi-grid-fill"></i>
+              <span className="label">Internal Link</span>
+              <i className="bi bi-chevron-right ms-auto"></i>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link d-flex align-items-center" to="/location">
+              <i className="bi bi-grid-fill"></i>
+              <span className="label">Internal Link</span>
+              <i className="bi bi-chevron-right ms-auto"></i>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link d-flex align-items-center" to="/location">
+              <i className="bi bi-grid-fill"></i>
+              <span className="label">Internal Link</span>
+              <i className="bi bi-chevron-right ms-auto"></i>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link d-flex align-items-center" to="/location">
+              <i className="bi bi-grid-fill"></i>
+              <span className="label">Internal Link</span>
+              <i className="bi bi-chevron-right ms-auto"></i>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link d-flex align-items-center" to="/location">
+              <i className="bi bi-grid-fill"></i>
+              <span className="label">Internal Link</span>
+              <i className="bi bi-chevron-right ms-auto"></i>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link d-flex align-items-center" to="/location">
+              <i className="bi bi-grid-fill"></i>
+              <span className="label">Internal Link</span>
+              <i className="bi bi-chevron-right ms-auto"></i>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link d-flex align-items-center" to="/location">
+              <i className="bi bi-grid-fill"></i>
+              <span className="label">Internal Link</span>
+              <i className="bi bi-chevron-right ms-auto"></i>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link d-flex align-items-center" to="/location">
+              <i className="bi bi-grid-fill"></i>
+              <span className="label">Internal Link</span>
+              <i className="bi bi-chevron-right ms-auto"></i>
+            </Link>
           </li>
         </ul>
         <hr/>
-        <ul class="nav flex-column">
-          <p class="sidebar-navblock-header">
-            Logs
+        <ul className="nav flex-column">
+          <p className="sidebar-navblock-header">
+            Section Header
           </p>
-          <li class="nav-item">
-            <a class="nav-link d-flex align-items-center" href="/logs/windows">
-              <i class="bi bi-grid-fill"></i>
-              <span class="label">Windows</span>
-              <i class="bi bi-chevron-right ms-auto"></i>
+          <li className="nav-item">
+            <a className="nav-link d-flex align-items-center" href="/" target="_blank">
+              <i className="bi bi-grid-fill"></i>
+              <span className="label">External Link</span>
+              <i className="bi bi-box-arrow-up-right ms-auto"></i>
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link d-flex align-items-center" href="/logs/firewall">
-              <i class="bi bi-grid-fill"></i>
-              <span class="label">Firewall</span>
-              <i class="bi bi-chevron-right ms-auto"></i>
+          <li className="nav-item">
+            <a className="nav-link d-flex align-items-center" href="/" target="_blank">
+              <i className="bi bi-grid-fill"></i>
+              <span className="label">External Link</span>
+              <i className="bi bi-box-arrow-up-right ms-auto"></i>
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link d-flex align-items-center" href="/logs/isensor">
-              <i class="bi bi-grid-fill"></i>
-              <span class="label">iSensor</span>
-              <i class="bi bi-chevron-right ms-auto"></i>
+          <li className="nav-item">
+            <a className="nav-link d-flex align-items-center" href="/" target="_blank">
+              <i className="bi bi-grid-fill"></i>
+              <span className="label">External Link</span>
+              <i className="bi bi-box-arrow-up-right ms-auto"></i>
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link d-flex align-items-center" href="/logs/redcloak">
-              <i class="bi bi-grid-fill"></i>
-              <span class="label">Redcloak Events</span>
-              <i class="bi bi-chevron-right ms-auto"></i>
+          <li className="nav-item">
+            <a className="nav-link d-flex align-items-center" href="/" target="_blank">
+              <i className="bi bi-grid-fill"></i>
+              <span className="label">External Link</span>
+              <i className="bi bi-box-arrow-up-right ms-auto"></i>
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link d-flex align-items-center" href="/logs/lastline">
-              <i class="bi bi-grid-fill"></i>
-              <span class="label">Lastline Traffic</span>
-              <i class="bi bi-chevron-right ms-auto"></i>
+          <li className="nav-item">
+            <a className="nav-link d-flex align-items-center" href="/" target="_blank">
+              <i className="bi bi-grid-fill"></i>
+              <span className="label">External Link</span>
+              <i className="bi bi-box-arrow-up-right ms-auto"></i>
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link d-flex align-items-center" href="/logs/dns">
-              <i class="bi bi-grid-fill"></i>
-              <span class="label">DNS</span>
-              <i class="bi bi-chevron-right ms-auto"></i>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link d-flex align-items-center" href="/logs/vpn">
-              <i class="bi bi-grid-fill"></i>
-              <span class="label">VPN</span>
-              <i class="bi bi-chevron-right ms-auto"></i>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link d-flex align-items-center" href="/logs/web">
-              <i class="bi bi-grid-fill"></i>
-              <span class="label">Web Traffic</span>
-              <i class="bi bi-chevron-right ms-auto"></i>
-            </a>
-          </li>
-        </ul>
-        <hr/>
-        <ul class="nav flex-column">
-          <p class="sidebar-navblock-header">
-            Intelligence Accounts
-          </p>
-          <li class="nav-item">
-            <a class="nav-link d-flex align-items-center" href="#" target="_blank">
-              <i class="bi bi-grid-fill"></i>
-              <span class="label">Twitter</span>
-              <i class="bi bi-box-arrow-up-right ms-auto"></i>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link d-flex align-items-center" href="#" target="_blank">
-              <i class="bi bi-grid-fill"></i>
-              <span class="label">Facebook</span>
-              <i class="bi bi-box-arrow-up-right ms-auto"></i>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link d-flex align-items-center" href="#" target="_blank">
-              <i class="bi bi-grid-fill"></i>
-              <span class="label">Instagram</span>
-              <i class="bi bi-box-arrow-up-right ms-auto"></i>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link d-flex align-items-center" href="#" target="_blank">
-              <i class="bi bi-grid-fill"></i>
-              <span class="label">GSE</span>
-              <i class="bi bi-box-arrow-up-right ms-auto"></i>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link d-flex align-items-center" href="#" target="_blank">
-              <i class="bi bi-grid-fill"></i>
-              <span class="label">Linked-In</span>
-              <i class="bi bi-box-arrow-up-right ms-auto"></i>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link d-flex align-items-center" href="#" target="_blank">
-              <i class="bi bi-grid-fill"></i>
-              <span class="label">Gmail</span>
-              <i class="bi bi-box-arrow-up-right ms-auto"></i>
+          <li className="nav-item">
+            <a className="nav-link d-flex align-items-center" href="/" target="_blank">
+              <i className="bi bi-grid-fill"></i>
+              <span className="label">External Link</span>
+              <i className="bi bi-box-arrow-up-right ms-auto"></i>
             </a>
           </li>
         </ul>
--- src/components/UserMenuDropdown.jsx
+++ src/components/UserMenuDropdown.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function UserMenuDropdown() {
   return (
@@ -9,35 +9,35 @@ function UserMenuDropdown() {
         <span className="label">user name</span>
       </button>
       <div id="userSettingsDropdown" className="dropdown-menu dropdown-menu-end" aria-labelledby="userSettings">
-        <NavLink className="dropdown-item d-flex align-items-center" to="/users/id">
+        <Link className="dropdown-item d-flex align-items-center" to="/users/id">
           <i className="bi bi-person-badge-fill"></i>
           <span className="label">View Profile</span>
-        </NavLink>
+        </Link>
         <div className="dropdown-divider"></div>
         <h6 className="dropdown-header">Admin panel</h6>
-        <NavLink to="/users" className="dropdown-item d-flex align-items-center">
+        <Link to="/users" className="dropdown-item d-flex align-items-center">
           <i className="bi bi-people-fill"></i>
           <span className="label">User list</span>
-        </NavLink>
-        <NavLink className="dropdown-item d-flex align-items-center" to="/admin/accesslog">
+        </Link>
+        <Link className="dropdown-item d-flex align-items-center" to="/admin/accesslog">
           <i className="bi bi-unlock-fill"></i>
           <span className="label">Access log</span>
-        </NavLink>
-        <NavLink className="dropdown-item d-flex align-items-center" to="/admin/devblog">
+        </Link>
+        <Link className="dropdown-item d-flex align-items-center" to="/admin/devblog">
           <i className="bi bi-book-half"></i>
           <span className="label">Dev Blog</span>
-        </NavLink>
-        <NavLink className="dropdown-item d-flex align-items-center" to="/release-schedule" target="_blank">
+        </Link>
+        <Link className="dropdown-item d-flex align-items-center" to="/release-schedule" target="_blank">
           <i className="bi bi-github"></i>
           <span className="label">Release Schedule</span>
           <i className="bi bi-box-arrow-up-right ms-auto"></i>
-        </NavLink>
+        </Link>
         <div className="dropdown-divider"></div>
         <h6 className="dropdown-header">Settings</h6>
-        <NavLink className="dropdown-item d-flex align-items-center" to="/users/id/passwordchange">
+        <Link className="dropdown-item d-flex align-items-center" to="/users/id/passwordchange">
           <i className="bi bi-key-fill"></i>
           <span className="label">Change password</span>
-        </NavLink>
+        </Link>
         <div className="dropdown-divider"></div>
         <h6 className="dropdown-header">Preferences</h6>
         <form>
@@ -75,10 +75,10 @@ function UserMenuDropdown() {
           </div>
         </form>
         <div className="dropdown-divider"></div>
-        <NavLink className="dropdown-item d-flex align-items-center" to="/auth/logout">
+        <Link className="dropdown-item d-flex align-items-center" to="/auth/logout">
           <i className="bi bi-door-open-fill"></i>
           <span className="label">Log out</span>
-        </NavLink>
+        </Link>
       </div>
     </li>
   )
