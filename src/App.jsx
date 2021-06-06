dc31b67ef6ad5bca40be8e13ee5b00d5031acbc3
Lev Popov <levpopov1@gmail.com>
2021-06-06 23:05:01 +0100

    updates routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 // Views
+import Home from './views/Home';
 import PlayingCards from './views/PlayingCards';
 import CardGames from './views/CardGames';
 import TarotCards from './views/TarotCards';
-import Home from './views/Home';
 import Auth from './views/Auth';
 import NotFound from './views/NotFound';
 
@@ -22,22 +22,12 @@ function App() {
         <div className="grid-container">
           <Sidebar/>
           <Switch>
-            <Route path="/playing-cards">
-              <PlayingCards/>
-            </Route>
-            <Route path="/card-games">
-              <CardGames/>
-            </Route>
-            <Route path="/tarot-cards">
-              <TarotCards/>
-            </Route>
-            <Route path="/auth">
-              <Auth/>
-            </Route>
-            <Route exact path="/">
-              <Home/>
-            </Route>
-            <Route component={NotFound} />
+            <Route path="/playing-cards/:vendor" component={PlayingCards}></Route>
+            <Route path="/card-games/:vendor" component={CardGames}></Route>
+            <Route path="/tarot-cards" component={TarotCards}></Route>
+            <Route path="/auth" component={Auth}></Route>
+            <Route exact path="/" component={Home}></Route>
+            <Route component={NotFound}></Route>
           </Switch>
         </div>
       </div>
