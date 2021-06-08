6f54c3376e4bc80e35976c624efa86b023685361
Lev Popov <levpopov1@gmail.com>
2021-06-08 22:07:23 +0100

    bunhc of cahnges

--- src/views/PlayingCards.jsx
+++ src/views/PlayingCards.jsx
@@ -1,51 +1,41 @@
-import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchDecks, selectDeckByVendorName } from '../redux/slices/decksSlice';
-import { fetchVendors, setCurrentVendor, selectAllVendors} from '../redux/slices/vendorsSlice';
-import Deck from '../components/Deck';
+import { useSelector } from 'react-redux';
+import {useParams} from "react-router-dom";
+import { selectAllDecks } from '../redux/slices/decksSlice';
+import DeckList from '../components/DeckList';
 
 function PlayingCards() {
 
-  const routeParams = useParams();
-  const dispatch = useDispatch();
-  const vendors = useSelector(selectAllVendors);
-  const currentVendor = useSelector(state => state.vendors.currentVendor);
-  const decks = useSelector(state => selectDeckByVendorName(state, currentVendor.name));
-  // const deckStatus = useSelector(state => state.decks.status);
-  // const errorMessage = useSelector(state => state.decks.error);
-  // const vendorStatus = useSelector(state => state.vendors.status);
+  const params = useParams();
 
-  useEffect(() => {
-    dispatch(fetchDecks());
-  }, [dispatch]);
-    
-  useEffect(() => {
-    if(vendors.length !== 0){
-      dispatch(setCurrentVendor(routeParams.vendor));
+  let filter;
+
+  if(params.vendor){
+    filter = (state) => {
+      return state.decks.data.filter(deck => deck.vendor_slug === params.vendor);
     }
-  }, [dispatch, vendors, routeParams.vendor]);
+  }
+  else{
+    filter = selectAllDecks;
+  }
 
-  // let content;
+  const decks = useSelector(filter);
+  const error = useSelector(state => state.decks.error);
+  const status = useSelector(state => state.decks.status);
 
-  // if(deckStatus === 'loading'){
-  //   content = <div className="loader">Loading...</div>
-  // }
-  // else if(deckStatus === 'succeeded'){
-  //   content = decks.map(item => 
-  //     <p key={item._id}>{item.name}</p>
-  //   );
-  // }
-  // else if(deckStatus === 'failed'){
-  //   content = <div>{errorMessage}</div>
-  // }
+  let sideEffects;
+
+  if(status === 'loading'){
+    sideEffects = <div className="loader">Loading...</div>
+  }
+  else if(status === 'failed'){
+    sideEffects = <div>{error}</div>
+  }
 
   return (
     <div className="main">
       <div className="container-fluid">
-        <div className="row">
-          {decks.map(deck => <Deck key={deck._id} deck={deck}/>)}
-        </div>
+        {sideEffects}
+        <DeckList decks={decks}/>
       </div>
     </div>
   );
