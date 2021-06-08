4bd0c774d2d4b250a7f11ffa5dfc6d7578721780
Lev Popov <levpopov1@gmail.com>
2021-06-08 22:07:12 +0100

    adds fetch effect to start of app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
-// import { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { fetchVendors } from './redux/slices/vendorsSlice';
+import { fetchDecks } from './redux/slices/decksSlice';
 
 // Views
 import Home from './views/Home';
@@ -13,8 +15,22 @@ import NotFound from './views/NotFound';
 // components
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
+import VendorList from './components/VendorList';
+import DeckList from './components/DeckList';
+import CardList from './components/CardList';
+import Deck from './components/Deck';
 
 function App() {
+
+  const dispatch = useDispatch();
+  // const vendorStatus = useSelector(state => state.vendors.status);
+  // const deckStatus = useSelector(state => state.decks.status);
+
+  useEffect(() => {
+    dispatch(fetchVendors()); 
+    dispatch(fetchDecks()); 
+  }, [dispatch]);
+
   return (
     <Router>
       <div className="App">
@@ -22,7 +38,13 @@ function App() {
         <div className="grid-container">
           <Sidebar/>
           <Switch>
+            <Route path="/playing-cards/:vendor/:deck">
+              <CardList/>
+            </Route>
             <Route path="/playing-cards/:vendor">
+              <PlayingCards/>
+            </Route>
+            <Route path="/playing-cards">
               <PlayingCards/>
             </Route>
             <Route path="/card-games/:vendor">
