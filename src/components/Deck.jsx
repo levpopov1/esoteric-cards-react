829f74d8c94ed4679083530416fa7cb3023e55c0
Lev Popov <levpopov1@gmail.com>
2021-06-08 22:06:16 +0100

    adds links

--- src/components/Deck.jsx
+++ src/components/Deck.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
 
 function Deck({deck}) {
   return (
-    <div className="col-sm-3 my-3">
+  <div className="col-sm-3 my-3">
     <div className="card">
       <img src={deck.img} className="card-img-top" alt=""/>
       <div className="card-body">
         <h5 className="card-title">
-          {deck.name}
+          <Link to={'/' + deck.category_slug + '/' + deck.vendor_slug + '/' + deck.slug}>
+            {deck.name}
+          </Link>
         </h5>
+      </div>
+      <div className="card-footer">
+        <Link to={'/' + deck.category_slug + '/' + deck.vendor_slug}>
+          {deck.vendor}
+        </Link>
       </div>
     </div>
   </div>
