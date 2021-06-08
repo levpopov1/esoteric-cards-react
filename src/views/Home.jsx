d748d79d9372761221eb5b804655414c6c2953c3
Lev Popov <levpopov1@gmail.com>
2021-06-08 22:06:07 +0100

    removes some junk

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,6 +1,6 @@
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchDecks, selectAllDecks } from '../redux/slices/decksSlice';
+// import { useEffect } from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { fetchDecks, selectAllDecks } from '../redux/slices/decksSlice';
 
 function Home() {
   // const dispatch = useDispatch();
