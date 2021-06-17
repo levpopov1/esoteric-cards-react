26ba9346bce2175ec5d281689c2c9d350daa6986
Lev Popov <levpopov1@gmail.com>
2021-06-17 19:58:49 +0100

    returns Promise instead of object

--- src/redux/makeAPIRequest.js
+++ src/redux/makeAPIRequest.js
@@ -1,12 +1,16 @@
 const API_URL = 'http://localhost:5000/api/v1';
 
-export default async function makeAPIRequest(endpoint, requestOptions = {}){
+export default async function makeAPIRequest(endpoint, requestOptions = {}, accessToken=""){
+
+
+  console.log("acctoke", accessToken);
 
   if(Object.keys(requestOptions).length === 0){
     requestOptions = {
       method: 'GET',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'authorization': `bearer ${accessToken}`
       }
     }
   }
@@ -15,14 +19,27 @@ export default async function makeAPIRequest(endpoint, requestOptions = {}){
     const response = await fetch(API_URL + endpoint, requestOptions);
     const data = await response.json();
 
-    if(!response.ok){
-      return {error: data.errors, data: null}
-      // throw new Error(`API request error. Status: ${response.status} - ${response.statusText}`);
+    console.log(response);
+    console.log(data);
+    
+    if(data.errors){
+      return Promise.reject(data.errors);
     }
-    return {error: null, data: data};
+
+    if(!response.ok){
+      console.log("we are not ok")
+      // return Promise.reject({error: data.errors, data: null});      
+      // return Promise.reject(data.errors);
+      throw new Error(response.statusText);
+    }
+
+    // return Promise.resolve({error: null, data: data});    
+    return Promise.resolve(data);
   } 
   catch (error) {
-    return {error: error, data: null}
+    console.log("running catch")
+    // return Promise.reject({error: error, data: null});    
+    return Promise.reject(error);
   }
 
 }
