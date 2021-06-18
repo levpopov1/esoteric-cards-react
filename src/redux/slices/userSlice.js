22da7fcd06628f092320023c05aeb6b48f3ce97d
Lev Popov <levpopov1@gmail.com>
2021-06-19 00:21:46 +0100

    adds extra reducers

--- src/redux/slices/userSlice.js
+++ src/redux/slices/userSlice.js
@@ -1,9 +1,15 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import makeAPIRequest from '../../lib/makeAPIRequest';
+
+// Async Thunks
+export const loginUser = createAsyncThunk('user/loginUser', async (requestOptions) => makeAPIRequest("/decks", requestOptions));
 
 // Slice
 const userSlice = createSlice({
   name: 'user',
   initialState: {
+    error: null,
+    status: "idle",
     accessToken: ""
   },
   reducers: {
@@ -12,6 +18,19 @@ const userSlice = createSlice({
     },
     clearAccessToken: (state, action) => {
       state.accessToken = "";
+    }
+  },
+  extraReducers: {
+    [loginUser.pending]: (state, action) => {
+      state.status = 'loading';
+    },
+    [loginUser.fulfilled]: (state, action) => {
+      state.status = 'succeeded';
+      state.accessToken = action.payload;
+    },
+    [loginUser.rejected]: (state, action) => {
+      state.status = 'failed';
+      state.error = action.error.message;
     }
   }
 });
