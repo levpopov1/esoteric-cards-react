021f9efad03cd8ccba3591fd207054874f304c52
Lev Popov <levpopov1@gmail.com>
2021-06-23 19:16:57 +0100

    adds breadcrumbs builder

---
+++ src/components/Breadcrumbs.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+function PathChunk({chunks, name, current}) {
+  return (
+    <li className="nav-item">
+      <Link to={"/" + chunks.join("/")} className={`nav-link path-chunk ${current ? "fw-bold" : ""}`} aria-current={current ? "true" : "false"}>
+        {name}
+      </Link>
+    </li>
+  )
+}
+
+function Separator() {
+  return (
+    <li className="nav-item separator">
+      <span className="nav-link">
+        <i className="bi bi-arrow-right"></i>
+      </span>
+    </li>
+  )
+}
+
+function Home() {
+  return (
+    <li className="nav-item">
+      <Link to="/" className="nav-link">
+        <i className="bi bi-house-door"></i>
+        <span className="ms-2">Home</span>
+      </Link>
+    </li>
+  )
+}
+
+function Breadcrumbs() {
+
+  const location = useLocation();
+  const [crumbs, setCrumbs] = useState([]);
+
+  useEffect(() => {
+    const locs = location.pathname.split('/').slice(1);
+
+    const blocks = locs.map(function(item, index, locs){
+      return [
+        <PathChunk 
+          key={"i"+index}
+          chunks={locs.slice(0, index + 1)} 
+          name={item}
+          current={Boolean(index === locs.length -1)}
+        />,
+        <Separator key={"s"+index}/>
+      ]
+    });
+
+    setCrumbs([...blocks.flat().slice(0, -1)]);
+
+  }, [location]);
+
+  return (
+    <div className="breadcrumbs">
+      <ul className="nav" aria-label="breadcrumb">
+        <Home/>
+        <Separator/>
+        {crumbs}
+      </ul>
+    </div>
+  )
+}
+
+export default Breadcrumbs;
+
+
+
