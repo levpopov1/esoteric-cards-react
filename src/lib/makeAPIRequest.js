ba3a24acb5ef05ca8df773341def12ea0bb9ab8a
Lev Popov <levpopov1@gmail.com>
2021-06-24 18:12:57 +0100

    refactor to use axios

--- src/lib/makeAPIRequest.js
+++ src/lib/makeAPIRequest.js
@@ -1,45 +1,41 @@
-const API_URL = 'http://localhost:5000/api/v1';
+import axios from "axios";
 
-export default async function makeAPIRequest(endpoint, requestOptions = {}, accessToken=""){
-
-
-  console.log("acctoke", accessToken);
-
-  if(Object.keys(requestOptions).length === 0){
-    requestOptions = {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'authorization': `bearer ${accessToken}`
-      }
-    }
+const instance = axios.create({
+  baseURL: 'http://localhost:5000/api/v1',
+  credentials: 'include',
+  headers: {
+    'Content-Type': 'application/json'
   }
+});
 
-  try {
-    const response = await fetch(API_URL + endpoint, requestOptions);
-    const data = await response.json();
-
-    console.log(response);
-    console.log(data);
-    
-    if(data.errors){
-      return Promise.reject(data.errors);
-    }
-
-    if(!response.ok){
-      console.log("we are not ok")
-      // return Promise.reject({error: data.errors, data: null});      
-      // return Promise.reject(data.errors);
-      throw new Error(response.statusText);
-    }
-
-    // return Promise.resolve({error: null, data: data});    
-    return Promise.resolve(data);
-  } 
-  catch (error) {
-    console.log("running catch")
-    // return Promise.reject({error: error, data: null});    
-    return Promise.reject(error);
-  }
-
+const setInstanceHeader = (headerName, headerValue) => {
+  instance.defaults.headers.common[headerName] = headerValue;
 }
+
+const handleReqeustError = (errors) => {
+  return Promise.reject(errors);
+}
+
+const handleRequesSuccess = (response) => {
+  if(response.data.errors){
+    return Promise.reject(response.data.errors);
+  }
+  return Promise.resolve(response.data);
+}
+
+const API = {
+  get: (url) => 
+    instance.get(url).then(response => handleRequesSuccess(response)).catch(errors => handleReqeustError(errors)),
+  post: (url, data) => 
+    instance.post(url, data).then(response => handleRequesSuccess(response)).catch(errors => handleReqeustError(errors)),
+  put: (url, data) => 
+    instance.put(url, data).then(response => handleRequesSuccess(response)).catch(errors => handleReqeustError(errors)),
+  patch: (url, data) => 
+    instance.patch(url, data).then(response => handleRequesSuccess(response)).catch(errors => handleReqeustError(errors)),
+  delete: (url) => 
+    instance.delete(url).then(response => handleRequesSuccess(response)).catch(errors => handleReqeustError(errors)),
+  setToken: (token) =>
+    setInstanceHeader('Authorization', `Bearer ${token}`)
+}
+
+export default API;
