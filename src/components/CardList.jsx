a72259c86d432d95543aad4dfa8f4d98e7383062
Lev Popov <levpopov1@gmail.com>
2021-07-05 18:25:36 +0100

    cardlist and view now support many list providers

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -3,64 +3,54 @@ import { useSelector } from 'react-redux';
 import {useParams} from "react-router-dom";
 import Card from './Card';
 
-function CardList(props) {
+function CardList({currentDeck, children}) {
 
-  const params = useParams();
-  const [currentDeck, setCurrentDeck] = useState({
-    cards: []
-  });
-
+  const [sideEffects, setSideEffects] = useState(null);
   const [showCardBack, setShowCardBack] = useState(false);
-  
-  const storeDeck = useSelector(state => state.decks.data.find(deck => deck.vendor_slug === params.vendor && deck.slug === params.deck));
   const error = useSelector(state => state.decks.error);
   const status = useSelector(state => state.decks.status);
 
   useEffect(() => {
-    if(storeDeck){
-      setCurrentDeck(storeDeck);
-    }
-  }, [storeDeck]);
-
-  useEffect(() => {
-    if(props.deck){
-      setCurrentDeck(props.deck);
-    }
-  }, [props.deck])
-
-  const sideEffects = () => {
     switch (status) {
       case "loading":
-        return <div className="loader text-center p-3">Loading...</div>;
+        setSideEffects(<div className="loader text-center p-3">Loading...</div>);
+        break;
       case "failed":
-        return <div>{error}</div>;
+        setSideEffects(<div>{error}</div>);
+        break;
       case "succeeded":
-        if(!storeDeck){
-          return <div className="text-center p-3">Deck not found.</div>
+        if(!currentDeck){
+          setSideEffects(<div className="text-center p-3">Deck not found.</div>);
         }
-        else if(storeDeck.cards.length === 0){
-          return <div className="text-center p-3">No cards in this deck.</div>
+        else if(currentDeck.cards.length === 0){
+          setSideEffects(<div className="text-center p-3">No cards in this deck.</div>);
+        }
+        else{
+          setSideEffects(null);
         }
         break;
       default:
+        setSideEffects(null);
         break;
     }
-  }
+  }, [status, error, currentDeck]);
 
   return (
     <div className="row">
-      {sideEffects()}
-      {
-        status === 'succeeded' && currentDeck.name &&
-        <div className="col-sm-12">
-          <div className="d-flex flex-row justify-content-between align-items-center">
-            <h1 className="display-5 my-3">{currentDeck.vendor + " | " + currentDeck.name}</h1>
-            <button className="btn btn-primary" onClick={() => setShowCardBack(prev => !prev)}>
-              Flip All
-            </button>
-          </div>
+      <div className="col-sm-12">
+        <div className="d-flex flex-row justify-content-between align-items-center">
+          <h1 className="display-5 my-3">
+            {currentDeck.vendor + " | " + currentDeck.name}
+          </h1>
+          <button className="btn btn-primary" onClick={() => setShowCardBack(prev => !prev)}>
+            Flip All
+          </button>
         </div>
-      }
+        {sideEffects}
+      </div>
+      <div className="col-sm-12 d-flex justify-content-center">
+        {children}
+      </div>
       {
         currentDeck.cards.map(card => <Card key={card.card_id} card={card} showCardBack={showCardBack}/>)
       }
--- src/views/CardsView.jsx
+++ src/views/CardsView.jsx
@@ -1,11 +1,20 @@
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import {useParams} from "react-router-dom";
+import {selectDeckByRouteParams} from '../redux/slices/decksSlice';
+
 import Breadcrumbs from '../components/Breadcrumbs';
 import CardList from '../components/CardList';
 
 function CardsView() {
+
+  const params = useParams();
+  const currentDeck = useSelector(state => selectDeckByRouteParams(state, params));
+
   return (
     <div className="container-fluid">
       <Breadcrumbs/>
-      <CardList/>
+      <CardList currentDeck={currentDeck}/>
     </div>
   );
 }
